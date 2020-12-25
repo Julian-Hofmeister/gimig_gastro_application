df8723e8d1194c16fb4e7c6caf0ee5b0cc323abe
Julian-Hofmeister <ju.hofmeister@web.de>
2020-12-25 15:47:07 +0100

    23.11 online menu Update

---
+++ lib/daily_menu/food_class.dart
@@ -0,0 +1,23 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class Food {
+  String id;
+  String name;
+  String image;
+
+  Food();
+
+  Food.fromMap(Map<String, dynamic> data) {
+    id = data['id'];
+    name = data['name'];
+    image = data['image'];
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+      'image': image,
+    };
+  }
+}
